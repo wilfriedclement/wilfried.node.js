965cdc0062f68c58a0d905ce1fc3c53e8ebbb181
Your Name <you@example.com>
2024-06-23 18:45:53 +0200

    1

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
+/**
+    * @description      : 
+    * @author           : Hp
+    * @group            : 
+    * @created          : 23/06/2024 - 18:40:13
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 23/06/2024
+    * - Author          : Hp
+    * - Modification    : 
+**/
 // src/app.ts
 
 import { envs } from './core/config/env';
 import app from './server';
 
-app.get('/', (_req, res) => {
-	res.send({
-		message: `Test done well !`
-	});
-});
+// app.get('/', (_req, res) => {
+// 	res.send({
+// 		message: `Test done well !`
+// 	});
+// });
 
 app.listen(envs.PORT, () => {
 	console.log(`Server running on port http://localhost:${envs.PORT}/`);
